31c9784f9549943aa284054b5084d58b88b60f12
tanayakarmakar <karmakar.tanaya@gmail.com>
2025-06-18 21:10:51 +0530

    langgraph

---
+++ langgraph/langchain_agent.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.agents import initialize_agent, AgentType
+from tools.tavily_search import search_tool
+from tools.system_time import get_system_time
+
+load_dotenv()
+
+llm = ChatOpenAI()
+
+tools = [search_tool, get_system_time]
+
+agent = initialize_agent(
+    tools = tools,
+    llm = llm,
+    agent = AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    verbose = True
+)
+
+result = agent.invoke("When was SpaceX's last launch and how many days ago was that from this instance")
+
+print(result)
+
+
+
